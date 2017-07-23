4b5b727918067639cf39f16222cbf5d1c316abf2
Trey Duffy <trey@flightlessnerds.com>
2017-07-22 20:58:02 -0700

    quick update to check for redecleration of functions

--- controller.php
+++ controller.php
@@ -1,52 +1,73 @@
 <?php
 /*
-Plugin Name:        ISP Blade Controller
+Plugin Name:        ISP Blace Controller
 Plugin URI:         https://github.com/treyduffy/insyncplus
 Description:        ISP Customized Blade Controller Plugin
 Version:            9.0.4
-Author:             Flightless Nerds | Darren Jacoby
+Author:             Flightless Nerds
 Author URI:         http://github.com/treyduffy/
 License:            MIT License
 License URI:        http://opensource.org/licenses/MIT
-GitHub Plugin URI:  treyduffy/controller
+GitHub Plugin URI:  treyduffy/insyncplus
 GitHub Branch:      master
 */
 
 namespace Sober\Controller;
 
 /**
- * Functions
+ * Plugin
  */
-function loader()
-{
-    $loader = new Loader();
-    foreach ($loader->getData() as $template => $class) {
-	    $class::employ();
-        // Pass data filter
-        add_filter('sage/template/' . $template . '-data/data', function ($data) use ($loader, $class) {
-            $controller = new $class();
-            $controller->__setup();
-            return array_merge($loader->getAppData(), $loader->getPostData(), $controller->__setTreeData($data), $controller->__getData());
-        });
-        // Class alais
-        class_alias($class, (new \ReflectionClass($class))->getShortName());
-    }
-}
+if (!defined('ABSPATH')) {
+	die;
+};
 
-function debugger()
-{
-    if (function_exists('\\App\\sage')) {
-        \App\sage('blade')->compiler()->directive('debug', function ($type) {
-            $debugger = ($type === '' ? '"controller"' : $type);
-            return '<?php (new \Sober\Controller\Module\Debugger(get_defined_vars(), ' .  $debugger . ')); ?>';
-        });
-    }
-}
+$require = file_exists( $composer = __DIR__ . '/vendor/autoload.php' ) ? $composer : __DIR__ . '/dist/autoload.php';
+require_once( $require );
 
 /**
- * Hooks
+ * Functions
  */
-if (function_exists('add_action')) {
-    add_action('init', __NAMESPACE__ . '\loader');
-    add_action('init', __NAMESPACE__ . '\debugger');
+if( !function_exists("Sober\\Controller\loader") ) {
+
+	function loader() {
+		$loader = new Loader();
+		foreach ( $loader->getData() as $template => $class ) {
+			$class::employ();
+			// Pass data filter
+			add_filter( 'sage/template/' . $template . '-data/data',
+				function ( $data ) use ( $loader, $class ) {
+					$controller = new $class();
+					$controller->__setup();
+
+					return array_merge( $loader->getAppData(),
+						$loader->getPostData(),
+						$controller->__setTreeData( $data ),
+						$controller->__getData() );
+				} );
+			// Class alais
+			class_alias( $class, ( new \ReflectionClass( $class ) )->getShortName() );
+		}
+	}
+
+	if ( function_exists( 'add_action' ) ) {
+		add_action( 'init', __NAMESPACE__ . '\loader' );
+	}
+}
+
+if( !function_exists( "Sober\\Controller\debugger" ) ) {
+
+	function debugger() {
+		if ( function_exists( '\\App\\sage' ) ) {
+			\App\sage( 'blade' )->compiler()->directive( 'debug',
+				function ( $type ) {
+					$debugger = ( $type === '' ? '"controller"' : $type );
+
+					return '<?php (new \Sober\Controller\Module\Debugger(get_defined_vars(), ' . $debugger . ')); ?>';
+				} );
+		}
+	}
+
+	if ( function_exists( 'add_action' ) ) {
+		add_action( 'init', __NAMESPACE__ . '\debugger' );
+	}
 }
