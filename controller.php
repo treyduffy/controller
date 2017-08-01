6a399e4ee41b2640856dfbf0ee3cf9797980d526
Trey Duffy <trey@flightlessnerds.com>
2017-08-01 01:08:01 -0700

    some instance checking

--- controller.php
+++ controller.php
@@ -17,9 +17,13 @@ namespace Sober;
 /**
  * Plugin
  */
-if (!defined('ABSPATH') || class_exists("Sober\\Controller\\Controller", false) ) {
-	die;
+use Sober\Controller\Loader;
+
+if (!defined('ABSPATH') ) {
+  die;
 };
+
+if ( function_exists( 'add_action' ) && !function_exists( "Sober\\loader" ) && !function_exists( "Sober\\debugger" ) ) :
 
 $require = file_exists( $composer = __DIR__ . '/vendor/autoload.php' ) ? $composer : __DIR__ . '/dist/autoload.php';
 require_once( $require );
@@ -27,47 +31,38 @@ require_once( $require );
 /**
  * Functions
  */
-if( !function_exists("Sober\\Controller\loader") ) {
+function loader() {
+  $loader = new Loader();
+  foreach ( $loader->getData() as $template => $class ) {
+    $class::employ();
+    // Pass data filter
+    add_filter( 'sage/template/' . $template . '-data/data',
+      function ( $data ) use ( $loader, $class ) {
+        $controller = new $class();
+        $controller->__setup();
 
-	function loader() {
-		$loader = new Loader();
-		foreach ( $loader->getData() as $template => $class ) {
-			$class::employ();
-			// Pass data filter
-			add_filter( 'sage/template/' . $template . '-data/data',
-				function ( $data ) use ( $loader, $class ) {
-					$controller = new $class();
-					$controller->__setup();
-
-					return array_merge( $loader->getAppData(),
-						$loader->getPostData(),
-						$controller->__setTreeData( $data ),
-						$controller->__getData() );
-				} );
-			// Class alais
-			class_alias( $class, ( new \ReflectionClass( $class ) )->getShortName() );
-		}
-	}
-
-	if ( function_exists( 'add_action' ) ) {
-		add_action( 'init', __NAMESPACE__ . '\loader' );
-	}
+        return array_merge( $loader->getAppData(),
+          $loader->getPostData(),
+          $controller->__setTreeData( $data ),
+          $controller->__getData() );
+      } );
+    // Class alais
+    class_alias( $class, ( new \ReflectionClass( $class ) )->getShortName() );
+  }
 }
+add_action( 'init', __NAMESPACE__ . '\loader' );
 
-if( !function_exists( "Sober\\Controller\debugger" ) ) {
 
-	function debugger() {
-		if ( function_exists( '\\App\\sage' ) ) {
-			\App\sage( 'blade' )->compiler()->directive( 'debug',
-				function ( $type ) {
-					$debugger = ( $type === '' ? '"controller"' : $type );
+function debugger() {
+  if ( function_exists( '\\App\\sage' ) ) {
+    \App\sage( 'blade' )->compiler()->directive( 'debug',
+      function ( $type ) {
+        $debugger = ( $type === '' ? '"controller"' : $type );
 
-					return '<?php (new \Sober\Controller\Module\Debugger(get_defined_vars(), ' . $debugger . ')); ?>';
-				} );
-		}
-	}
-
-	if ( function_exists( 'add_action' ) ) {
-		add_action( 'init', __NAMESPACE__ . '\debugger' );
-	}
+        return '<?php (new \Sober\Controller\Module\Debugger(get_defined_vars(), ' . $debugger . ')); ?>';
+      } );
+  }
 }
+add_action( 'init', __NAMESPACE__ . '\debugger' );
+
+endif;
